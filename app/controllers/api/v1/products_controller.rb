98df614c4da661d9e145ce2358df4fb6f4a3eba2
Mao Li <anrenyil@gmail.com>
2019-09-26 11:28:23 -0700

    API product update destroy

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < Api::ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :find_product, only: [:show, :destroy]
+  before_action :find_product, only: [:show, :destroy, :update]
 
     def index
       products = Product.order(created_at: :desc)
@@ -24,7 +24,22 @@ class Api::V1::ProductsController < Api::ApplicationController
       end
     end
 
+    def destroy
+      @product.destroy
+      render json: { status: 200 }, status: 200
+    end
 
+    def update
+      if @product.update product_params
+        render json: @product
+      else
+        render(
+          json: { errors: product.errors },
+          status: 422 
+        )
+      end
+    end
+    
     private
   
     def find_product
